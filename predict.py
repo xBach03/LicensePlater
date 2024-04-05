18fa75c8a2eae8baf9b566b6cbbcad61b1896dad
xBach03 <dangxuanbach03@gmail.com>
2024-04-05 17:24:44 +0700

    update scan feature

--- predict.py
+++ predict.py
@@ -2,42 +2,91 @@ import cv2 as cv
 from visualize import readLicensePlate
 from ultralytics import YOLO
 
-# Define the image path
-image_path = r'D:\VsCode\dataset(origin)\dataset\test\images\xemay1223_jpg.rf.c27d717c455451a3b01e7283324a1faf.jpg'
+# # Define the image path
+# image_path = r'D:\VsCode\dataset(origin)\dataset\test\images\xemay1223_jpg.rf.c27d717c455451a3b01e7283324a1faf.jpg'
 
 # Load the YOLO model
-model_path = r'D:\VsCode\LicensePlate\runs\detect\train2\weights\last.pt'
+model_path = r'D:\VS Code\LicensePlater\runs\detect\train2\weights\last.pt'
 model = YOLO(model_path)
 
 # Read image 
-image = cv.imread(image_path)
+# image = cv.imread(image_path)
 
-# Perform inference 
-results = model(image)[0]
-for detection in results.boxes.data.tolist():
-    x1, y1, x2, y2, score, class_id = detection
+# result = model(source=0, show=True, conf=0.3, save=False)
 
-    # Crop license plate
-    licensePlateCrop = image[int(y1):int(y2), int(x1):int(x2), :]
+
+# Initialize camera
+cap = cv.VideoCapture(0)  # 0 represents the default camera (you can change it if needed)
+
+while True:
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    if not ret:
+        print("Failed to capture frame")
+        break
+
+    # Perform detection
+    result = model(frame)[0]
+
+    # Assuming you have access to the detected region of the license plate
+    for detection in result.boxes.data.tolist():
+        x1, y1, x2, y2, score, class_id = detection
+
+        # Crop license plate
+        licensePlateCrop = frame[int(y1):int(y2), int(x1):int(x2), :]
+        
+        # Process license plate
+        licensePlateCropGray = cv.cvtColor(licensePlateCrop, cv.COLOR_BGR2GRAY)
+        # _, licensePlateCropThresh = cv.threshold(licensePlateCropGray, 64, 255, cv.THRESH_BINARY_INV)
+
+
+        # Read license plate
+        licensePlateText, confidenceScore = readLicensePlate(licensePlateCropGray)
     
-    # Process license plate
-    licensePlateCropGray = cv.cvtColor(licensePlateCrop, cv.COLOR_BGR2GRAY)
-    _, licensePlateCropThresh = cv.threshold(licensePlateCropGray, 110, 255, cv.THRESH_BINARY_INV)
+
+        # Draw rectangle detector
+        cv.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 2)
+
+        # Print license plate
+        cv.putText(frame, licensePlateText, (int(x1), int(y1) - 10), cv.FONT_HERSHEY_SIMPLEX, 1.5, (0, 0, 255), 2)
+        # Display the resulting frame
+    cv.imshow('Frame', frame)
+
+    # Press 'q' to exit
+    if cv.waitKey(10) & 0xFF == ord('q'):
+        break
+
+# Release the capture
+cap.release()
+cv.destroyAllWindows()
+
+# Read license plate
+# Perform inference 
+# results = model(image)[0]
+# for detection in results.boxes.data.tolist():
+#     x1, y1, x2, y2, score, class_id = detection
+
+#     # Crop license plate
+#     licensePlateCrop = image[int(y1):int(y2), int(x1):int(x2), :]
+    
+#     # Process license plate
+#     licensePlateCropGray = cv.cvtColor(licensePlateCrop, cv.COLOR_BGR2GRAY)
+#     _, licensePlateCropThresh = cv.threshold(licensePlateCropGray, 110, 255, cv.THRESH_BINARY_INV)
 
 
-    # Read license plate
-    licensePlateText, confidenceScore = readLicensePlate(licensePlateCropThresh)
+#     # Read license plate
+#     licensePlateText, confidenceScore = readLicensePlate(licensePlateCropThresh)
 
-    # Draw rectangle detector
-    cv.rectangle(image, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 2)
+#     # Draw rectangle detector
+#     cv.rectangle(image, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 2)
 
-    # Print license plate
-    cv.putText(image, licensePlateText, (int(x1), int(y1) - 10), cv.FONT_HERSHEY_SIMPLEX, 1.5, (0, 0, 255), 2)
+#     # Print license plate
+#     cv.putText(image, licensePlateText, (int(x1), int(y1) - 10), cv.FONT_HERSHEY_SIMPLEX, 1.5, (0, 0, 255), 2)
 
-# Display the annotated image
-cv.imshow("License Plate Detection", image)
-cv.waitKey(0)
-cv.destroyAllWindows() 
+# # Display the annotated image
+# cv.imshow("License Plate Detection", image)
+# cv.waitKey(0)
+# cv.destroyAllWindows() 
 
 
 
--- visualize.py
+++ visualize.py
@@ -4,57 +4,56 @@ import easyocr
 # Initialize the OCR reader
 reader = easyocr.Reader(['en'], gpu=False)
 
-# Mapping dictionaries for character conversion
-dict_char_to_int = {'O': '0',
-                    'I': '1',
-                    'J': '3',
-                    'A': '4',
-                    'G': '6',
-                    'S': '5'}
+# # Mapping dictionaries for character conversion
+# dict_char_to_int = {'O': '0',
+#                     'I': '1',
+#                     'J': '3',
+#                     'A': '4',
+#                     'G': '6',
+#                     'S': '5'}
 
-dict_int_to_char = {'0': 'O',
-                    '1': 'I',
-                    '3': 'J',
-                    '4': 'A',
-                    '6': 'G',
-                    '5': 'S'}
-numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+# dict_int_to_char = {'0': 'O',
+#                     '1': 'I',
+#                     '3': 'J',
+#                     '4': 'A',
+#                     '6': 'G',
+#                     '5': 'S'}
+# numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
-# Checkers
-def checkNums(text):
-    if text in numbers or text in dict_char_to_int.keys():
-        return True
-    return False
+# # Checkers
+# def checkNums(text):
+#     if text in numbers or text in dict_char_to_int.keys():
+#         return True
+#     return False
 
-def checkChar(text):
-    if text in string.ascii_uppercase or text in dict_int_to_char.keys():
-        return True
-    return False
+# def checkChar(text):
+#     if text in string.ascii_uppercase or text in dict_int_to_char.keys():
+#         return True
+#     return False
 
-# Check if license format is correct
-def licenseFormat(text): 
-    if(checkNums(text[0]) and checkNums(text[1]) and checkChar(text[2])):
-        for char in text[4: ]:
-            if(char == "-" or char == "."):
-                continue
-            if(not checkNums(char)):
-                return False
-    else: 
-        return False;        
-    return True
+# # Check if license format is correct
+# def licenseFormat(text): 
+#     if(checkNums(text[0]) and checkNums(text[1]) and checkChar(text[2])):
+#         for char in text[4: ]:
+#             if(char == "-" or char == "."):
+#                 continue
+#             if(not checkNums(char)):
+#                 return False
+#     else: 
+#         return False;        
+#     return True
 
 # Read license plate
 def readLicensePlate(licensePlateCrop):
     resultLicense = ''
     scores = []
     detections = reader.readtext(licensePlateCrop)
-    for detection in detections:
-        bbox, text, score = detection
-        resultLicense += text
-        scores.append(score)
-    if(licenseFormat(resultLicense.replace(' ', ''))):
-        print(resultLicense)
-        return resultLicense, scores
+    if not detections:
+        print("No text detected")
     else:
-        print("Plate detected: " + resultLicense)
-        return None, None
+        for detection in detections:
+            bbox, text, score = detection
+            resultLicense += text
+            scores.append(score)
+            print("Detected text:", text)
+    return resultLicense, scores
