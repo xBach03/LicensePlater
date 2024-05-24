cf512b4dfd5d82757407283d38c01da3bf7b1bba
xBach <dangxuanbach03@gmail.com>
2024-05-24 22:55:07 +0700

    modify get api

--- webApi.py
+++ webApi.py
@@ -17,11 +17,11 @@ if not os.path.exists(weights_path):
 
 yolov8_model = YOLO(weights_path)
 
-licensePlateContent = ""
+save_path = ""
 
 @app.route("/", methods=['GET', 'POST'])
 def home_page():
-    global licensePlateContent
+    global save_path
     if request.method == "POST":
         try:
             # Read posted file
@@ -29,20 +29,19 @@ def home_page():
             if image:
                 # Save file
                 path_to_save = os.path.join(app.config['UPLOAD_FOLDER'], image.filename)
+                save_path = path_to_save
                 print("Save =", path_to_save)
                 image.save(path_to_save)
 
                 # Process image and detect license plates
                 processed_image, licensePlateText = process_image(path_to_save, weights_path)
-
-                licensePlateContent = licensePlateText
-
+                
                 # Save the image with bounding boxes
                 result_image_path = os.path.join(app.config['UPLOAD_FOLDER'], f"result_{image.filename}")
                 cv2.imwrite(result_image_path, processed_image)
 
                 if licensePlateText:
-                    return render_template("index.html", user_image=f"result_{image.filename}", rand=str(random()), msg="Content from number plate was extracted", ndet = 1)
+                    return render_template("index.html", user_image=f"result_{image.filename}", rand=str(random()), msg="Content from number plate was extracted and saved", ndet = 1)
                 else:
                     return render_template('index.html', user_image=f"result_{image.filename}", rand=str(random()), msg="Cannot extract content from number plate", ndet = 1)
             else:
@@ -60,26 +59,16 @@ def home_page():
 
 @app.route('/get-license', methods = ['GET'])
 def get_license_plate():
-    return jsonify({"licensePlateContent": licensePlateContent})
-# @app.route('/api/detect', methods=['POST'])
-# def predict_yolov8():
-#     image = request.files.get('file')
-#     if image:
-#         # Save file
-#         path_to_save = os.path.join(app.config['UPLOAD_FOLDER'], image.filename)
-#         print("Save =", path_to_save)
-#         image.save(path_to_save)
-
-#         # Process image and detect license plates
-#         processed_image, licensePlateText = process_image(path_to_save, weights_path)
-
-#         # Save the image with bounding boxes
-#         result_image_path = os.path.join(app.config['UPLOAD_FOLDER'], f"result_{image.filename}")
-#         cv2.imwrite(result_image_path, processed_image)
-
-#         return jsonify({"path": result_image_path, "licensePlateText": licensePlateText})
-
-#     return jsonify({"error": "Upload a file to detect"}), 400
+    # global licensePlateContent
+    # print("licensePlateContent: " + licensePlateContent)
+    # if licensePlateContent:
+    #     licenseResult = licensePlateContent
+    #     licensePlateContent = ""  # Reset the content after reading
+    #     return jsonify({"status": "complete", "licenseResult": licenseResult})
+    # else:
+    #     return jsonify({"status": "failed"})
+    processed_image, license_plate_content = process_image(save_path, weights_path)
+    return jsonify({"licensePlateContent": license_plate_content, "status": "success"})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=6868, debug=True)
